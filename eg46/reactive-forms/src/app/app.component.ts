beb6927337494f412b7af416374891acb1c258a6
Administrator <slyfox2046@gmail.com>
2020-02-08 10:58:52 +0800

    eg46

--- eg46/reactive-forms/src/app/app.component.ts
+++ eg46/reactive-forms/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  title = '响应式表单';
 }
--- eg46/reactive-forms/src/app/app.module.ts
+++ eg46/reactive-forms/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
