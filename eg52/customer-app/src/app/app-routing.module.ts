d48f6c6fa03497cdd212d858471c6a21265104cf
slyfox <slyfox2046@gmail.com>
2020-02-11 21:14:18 +0800

    eg52

---
+++ eg52/customer-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'customers', loadChildren: '../app/customers/customers.module#CustomersModule' },
+  { path: 'orders', loadChildren: '../app/orders/orders.module#OrdersModule' }, 
+  {path:"",redirectTo:'',pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
