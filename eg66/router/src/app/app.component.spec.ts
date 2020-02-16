2405d3664cc56737c6cbae3ae19f3bab5d9e40fc
slyfox <slyfox2046@gmail.com>
2020-02-16 14:20:39 +0800

    eg66

--- eg66/router/src/app/app.component.spec.ts
+++ eg66/router/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to router!');
+    expect(compiled.querySelector('h1').textContent).toContain('路由器示例');
   });
 });
